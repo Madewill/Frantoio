e96206213f6d3a494caa43f768218f035e1273c8
Madewill <ayomidegw@gmail.com>
2024-05-11 17:31:26 +0100

    Frantoio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import LanguageSwitcher from './Resource/LanguageSwitcher';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './components/Home';
 import Footer from './components/Footer';
 import Planting from './components/Planting';
 import Growing from './components/Growing';
@@ -9,6 +8,9 @@ import Harvesting from './components/Harvesting';
 import Crushing from './components/Crushing';
 import ColdPressing from './components/ColdPressing';
 import Bottling from './components/Bottling';
+import Main from './components/main';
+import Background from './components/Background';
+import Navigation from './components/Navigation';
 // import Frantoio from './Resource/Frantoio';
 
 
@@ -21,16 +23,27 @@ const App = () => {
       <LanguageSwitcher defaultLanguage='english'>
         {({ language, switchToEnglish, switchToArabic }) => (
           <BrowserRouter>
-            <>
-              <Home language={language} switchToEnglish={switchToEnglish} switchToArabic={switchToArabic} />
-              <Planting language={language} />
-              <Growing language={language} />
-              <Harvesting language={language} />
-              <Crushing language={language} />
-              <ColdPressing language={language} />
-              <Bottling language={language} />
-              <Footer language={language} s />
-            </>
+
+            <section className='overflow-hidden'>
+              <Navigation language={language} switchToEnglish={switchToEnglish} switchToArabic={switchToArabic} />
+              {/* Partitioning Bar */}
+              {/* position will be relative to the I am absolute, left top, so will be from base of div containing menu */}
+              <div className="bar mx-auto"></div>
+              <Main language={language} />
+
+              {/* Background video component controlled by index.css so it spreads across all sections */}
+              <Background />
+            </section>
+
+
+            <Planting language={language} />
+            <Growing language={language} />
+            <Harvesting language={language} />
+            <Crushing language={language} />
+            <ColdPressing language={language} />
+            <Bottling language={language} />
+            <Footer language={language} s />
+
           </BrowserRouter>
         )}
       </LanguageSwitcher>
