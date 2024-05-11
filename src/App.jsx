087de5443923ced85c09a151c2b8a6135fd56c38
Madewill <ayomidegw@gmail.com>
2024-05-11 15:41:30 +0100

    Frantoio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Index from './Resource/Frantoio';
-import Frantoio from './Resource/Frantoio';
+import LanguageSwitcher from './Resource/LanguageSwitcher';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Sections from './Resource/sections';
+import Footer from './components/Footer';
+// import Frantoio from './Resource/Frantoio';
 
 
 
@@ -8,7 +13,18 @@ const App = () => {
 
   return (
   <>
-    <Frantoio />
+    {/* <Frantoio /> */}
+    <LanguageSwitcher defaultLanguage='english'>
+                {({ language, switchToEnglish, switchToArabic }) => (
+                    <BrowserRouter>
+                        <>
+                            <Home language={language} switchToEnglish={switchToEnglish} switchToArabic={switchToArabic} />
+                            <Sections language={language} />
+                            <Footer language={language}s/>
+                        </>
+                    </BrowserRouter>
+                )}
+            </LanguageSwitcher>
   </>
     
   );
