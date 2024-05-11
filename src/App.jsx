d03e6598bfa216b97f3489836700f5a1e23c2e98
Madewill <ayomidegw@gmail.com>
2024-05-11 17:21:41 +0100

    Frantoio

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Sections from './Resource/sections';
 import Footer from './components/Footer';
+import Planting from './components/Planting';
+import Growing from './components/Growing';
+import Harvesting from './components/Harvesting';
+import Crushing from './components/Crushing';
+import ColdPressing from './components/ColdPressing';
+import Bottling from './components/Bottling';
 // import Frantoio from './Resource/Frantoio';
 
 
@@ -12,21 +18,26 @@ import Footer from './components/Footer';
 const App = () => {
 
   return (
-  <>
-    {/* <Frantoio /> */}
-    <LanguageSwitcher defaultLanguage='english'>
-                {({ language, switchToEnglish, switchToArabic }) => (
-                    <BrowserRouter>
-                        <>
-                            <Home language={language} switchToEnglish={switchToEnglish} switchToArabic={switchToArabic} />
-                            <Sections language={language} />
-                            <Footer language={language}s/>
-                        </>
-                    </BrowserRouter>
-                )}
-            </LanguageSwitcher>
-  </>
-    
+    <>
+      {/* <Frantoio /> */}
+      <LanguageSwitcher defaultLanguage='english'>
+        {({ language, switchToEnglish, switchToArabic }) => (
+          <BrowserRouter>
+            <>
+              <Home language={language} switchToEnglish={switchToEnglish} switchToArabic={switchToArabic} />
+              <Planting language={language} />
+              <Growing language={language} />
+              <Harvesting language={language} />
+              <Crushing language={language} />
+              <ColdPressing language={language} />
+              <Bottling language={language} />
+              <Footer language={language} s />
+            </>
+          </BrowserRouter>
+        )}
+      </LanguageSwitcher>
+    </>
+
   );
 };
 
